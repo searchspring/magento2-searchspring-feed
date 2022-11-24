85a5b35f16a3f05778987ed287be6070aec7276b
James Bathgate <james@searchspring.com>
2022-11-24 14:48:02 -0600

    Disable Category Cache

--- Helper/Generator.php
+++ Helper/Generator.php
@@ -617,7 +617,7 @@ class Generator extends \Magento\Framework\App\Helper\AbstractHelper
     {
         // TODO Ignore root categories? ex. Root Catalog
         // TODO Use a Magento 2 cache instead of a variable that is cleared on page 1.
-        if (!isset($this->categoryCache[$categoryId])) {
+        if (false && !isset($this->categoryCache[$categoryId])) {
             $category = $this->categoryRepository->get($categoryId);
             $categoryName = $category->getName();
             $categoryPath = $category->getPath();
